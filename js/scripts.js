3086da79f81947295e7dbfc8abeb36d338a8497c
Nicci Kaufman <niccikaufman@gmail.com>
2021-03-16 20:42:43 -0700

    create a function that splits any sentence entered in prompt into an array, another function that takes the first and last letter of the sentence, reverses and capitalizes them,  and returns a new string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
-function reverseString(str){
-  // First, the string elements are split up as individual arrays using the split() method. str.split("")
-  const arrayStrings = str.split("");
-  // Second, the string elements are reversed using the reverse() method. arrayStrings.reverse()
-  const reverseArray = arrayStrings.reverse();
-  // Third, the reversed string elements are joined into a single string using the join() method. reverseArray.join("")
-  const joinArray = reverseArray.join("");
-  return joinArray;
+let output = prompt("Say something: ");
+console.log(output.split(""));
+let firstLetter = output.charAt(0);
+let lastLetter = output.charAt(output.length-1);
+let bookEnd = firstLetter + lastLetter;
+
+function newString(){
+  console.log(bookEnd.toUpperCase());
 }
 
-const string = prompt('enter a sentence: ');
-const result = reverseString(string);
-alert(result);
+function reverseString(){
+  let reverse = lastLetter.concat(firstLetter).toUpperCase();
+  let result = output.concat(reverse);
+  console.log(result);
+}
+
+newString();
+reverseString();
